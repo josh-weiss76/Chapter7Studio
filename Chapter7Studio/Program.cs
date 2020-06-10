aec6c9ffe9907348e8f45c927ffe222061116be5
josh-weiss76 <59374753+josh-weiss76@users.noreply.github.com>
2020-06-10 15:32:40 -0400

    Add project files.

---
+++ Chapter7Studio/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Chapter7Studio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
